b22cfc174980943a5a0a904d7585eeeda456c364
D4Devil <dmdio1177@gmail.com>
2023-08-11 23:21:33 -0600

    first commit

---
+++ keymaps/default/keymap.c
@@ -0,0 +1,25 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┬───┬───┬───┐
+     * │ 7 │ 8 │ 9 │ / │
+     * ├───┼───┼───┼───┤
+     * │ 4 │ 5 │ 6 │ * │
+     * ├───┼───┼───┼───┤
+     * │ 1 │ 2 │ 3 │ - │
+     * ├───┼───┼───┼───┤
+     * │ 0 │ . │Ent│ + │
+     * └───┴───┴───┴───┘
+     */
+    [0] = LAYOUT_ortho_4x4(
+        KC_PDOT,   KC_G,   KC_T,
+        KC_V,   KC_F,   KC_R,
+        KC_C,   KC_D,   KC_E,
+        KC_X,   KC_S,   KC_W,
+        KC_Z,   KC_A,   KC_Q
+    )
+};
